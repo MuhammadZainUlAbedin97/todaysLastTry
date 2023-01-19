37626ed8079cbf06c1f7e77ee85be32a11de7d1a
MuhammadZainUlAbedin <muhammadzain6111@gmail.com>
2023-01-20 04:03:47 +0500

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+function Body() {
+	return (
+		<div>
+			<h1>Fun Facts about React</h1>
+			<ul>
+				<li>Was first released in 2013</li>
+				<li>Was orginally created by Jordan Walke</li>
+				<li>Has well over 100k stars on Github</li>
+				<li>Is maintained by facebook</li>
+				<li>Powers thousands of interprise apps, including mobile apps</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Body;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+	return (
+		<footer>
+			<small>
+				<i class="fa fa-copyright"></i> Zain development, All rights reserved
+			</small>
+		</footer>
+	);
+}
+
+export default Footer;
